051b62dec94e507e25af74f5ac4e6b9afea3979e
Muhamad Anik <anikbusinessman@gmail.com>
2020-11-25 11:44:20 +0700

    General Page

---
+++ lib/shared/theme.dart
@@ -0,0 +1,8 @@
+part of 'shared.dart';
+
+Color greyColor = "8D92A3".toColor();
+Color mainColor = "FFC700".toColor();
+
+TextStyle greyFontStyle = GoogleFonts.poppins().copyWith(color: greyColor);
+
+const double defaultMargin = 24;
